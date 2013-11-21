1859ff7f36cc81c77c34d044b5d6da24e5b4656d
Mimi Flynn <mimiflynn@gmail.com>
2013-11-21 12:22:12 -0500

    Added Search functionality

--- app/controller/Main.js
+++ app/controller/Main.js
@@ -4,11 +4,15 @@ Ext.define('WWTouch.controller.Main', {
     config: {
         refs: {
             blog: 'blog',
-            viewer: 'viewer'
+            viewer: 'viewer',
+            list: 'blog list'
         },
         control: {
             'blog': {
                 itemtap: 'showBlog'
+            },
+            'blog searchfield': {
+                keyup: 'onSearchKeyUp'
             }
         }
     },
@@ -39,5 +43,50 @@ Ext.define('WWTouch.controller.Main', {
         // set listener to do something after active item has changed
         this.getViewer().onAfter('activeitemchange', onActiveItemChange, this.getViewer());
 
+    },
+    onSearchKeyUp: function (field) {
+
+        console.log('onSearchKeyUp fired');
+
+        //get the store and the value of the field
+        var value = field.getValue(), store = this.getList().getStore();
+
+        //first clear any current filters on the store. If there is a new value, then suppress the refresh event
+        store.clearFilter(!!value);
+
+        //check if a value is set first, as if it isnt we dont have to do anything
+        if (value) {
+            //the user could have entered spaces, so we must split them so we can loop through them all
+            var searches = value.split(','), regexps = [], i, regex;
+
+            //loop them all
+            for (i = 0; i < searches.length; i++) {
+                //if it is nothing, continue
+                if (!searches[i])
+                    continue;
+
+                regex = searches[i].trim();
+                regex = regex.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+
+                //if found, create a new regular expression which is case insenstive
+                regexps.push(new RegExp(regex.trim(), 'i'));
+            }
+
+            //now filter the store by passing a method
+            //the passed method will be called for each record in the store
+            store.filter(function (record) {
+                var matched = [];
+
+                //loop through each of the regular expressions
+                for (i = 0; i < regexps.length; i++) {
+                    var search = regexps[i], didMatch = search.test(record.get('title') + ' ' + record.get('content'));
+
+                    //if it matched the first or last name, push it into the matches array
+                    matched.push(didMatch);
+                }
+
+                return (regexps.length && matched.indexOf(true) !== -1);
+            });
+        }
     }
 });
